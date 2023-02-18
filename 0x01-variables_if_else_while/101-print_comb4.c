32b93bbc6bccc242de08a171293b4888dc6a42ab
Fredric90 <fredricklurts5@gmail.com>
2023-02-18 08:26:59 -0800

    gel

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,81 +1,33 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - print numbers from 00 to 99.
- *
- *  (*
- *
- *   * Return: 0 on success
- *
- *    */
-
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int n, m, l;
 
-		int i = '0';
-
-			int j = '0';
-
-				int k = '0';
-
-
-
-					while (i <= '7')
-
-							{
-
-										while (j <= '8')
-
-													{
-
-																	while (k <= '9')
-
-																					{
-
-																										if (i < j && j < k)
-
-																															{
-
-																																					putchar(i);
-
-																																										putchar(j);
-
-																																															putchar(k);
-
-																																																				if (!(i == '7' && j == '8' && k == '9'))
-
-																																																										{
-
-																																																																	putchar(',');
-
-																																																																							putchar(' ');
-
-																																																																												}
-
-
-
-																																																								}
-
-																														k++;
-
-																																	}
-
-																				k = '0';
-
-																							j++;
-
-																									}
-
-												j = '0';
-
-														i++;
-
-															}
-
-						putchar('\n');
-
-							return (0);
-
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
